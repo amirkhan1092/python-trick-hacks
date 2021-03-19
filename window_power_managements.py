37863a003c93cfdc03f949cb77e84ffe6b0cb0f7
amirkhan1092 <amirkhan1092@gmail.com>
2021-03-20 00:34:40 +0530

    cpu count

--- window_power_managements.py
+++ window_power_managements.py
@@ -1,12 +1,20 @@
-'''
+"""
 the module psutil(process and system utilities) is a cross-platform
 library for retrieving information on running processes and system utilization
 (CPU, memory, disks, network, sensors) in Python. It is useful mainly for system monitoring,
 profiling and limiting process resources and management of running processes
-'''
+"""
 
 import psutil
+
+# battery power display
 power = psutil.sensors_battery()
 x = power.percent
 print(type(power))
 print(x, type(x))
+
+# cpu count
+n_cpu = psutil.cpu_count()
+print(n_cpu)
+
+
