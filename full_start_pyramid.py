2af2645f2036640554a457d021919c7ef27461e4
amirkhan1092 <amirkhan1092@gmail.com>
2021-03-22 16:50:46 +0530

    string triangle pattern in a single loop

---
+++ full_start_pyramid.py
@@ -0,0 +1,17 @@
+row = int(input('enter the number of rows '))
+
+# full star
+# for i in range(1, row+1):
+#     print(' '*(row-i)+'*'*(2*i-1))
+'''
+    *
+   ***
+  *****
+ *******
+*********
+'''
+
+# hollow triangle
+for i in range(1, row+1):
+    print(' '*(row-i) + '*' + ' '*(2*i-3) + '*'*(i != 1), end='\n'+'*'*(i==row and row*2) )
+
