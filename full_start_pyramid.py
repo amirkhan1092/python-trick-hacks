5e4b25bc5985cba784a6917e47935b1ba878cc18
amirkhan1092 <amirkhan1092@gmail.com>
2021-03-22 16:58:55 +0530

    triangle update

--- full_start_pyramid.py
+++ full_start_pyramid.py
@@ -1,8 +1,8 @@
 row = int(input('enter the number of rows '))
 
 # full star
-# for i in range(1, row+1):
-#     print(' '*(row-i)+'*'*(2*i-1))
+for i in range(1, row+1):
+    print(' '*(row-i)+'*'*(2*i-1))
 '''
     *
    ***
@@ -11,20 +11,3 @@ row = int(input('enter the number of rows '))
 *********
 '''
 
-# hollow triangle
-for i in range(1, row+1):
-    print(' ' * (row - i) + '*' + ' ' * (2 * i - 3) + '*' * (i != 1), end='\n' + '*' * (i == row and row * 2-1))
-'''
-Output 10
-         *
-        * *
-       *   *
-      *     *
-     *       *
-    *         *
-   *           *
-  *             *
- *               *
-*                 *
-******************** 
-'''
