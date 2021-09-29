6c1471641edd8df50b9930208f78a597afa7510e
amirkhan1092 <amirkhan1092@gmail.com>
2021-09-29 10:12:53 +0530

    triangle solution

--- hollow_tringle_single_loop.py
+++ hollow_tringle_single_loop.py
@@ -4,6 +4,7 @@ row = int(input('enter the number of rows '))
 
 for i in range(1, row+1):
     print(' ' * (row - i) + '*' + ' ' * (2 * i - 3) + '*' * (i != 1), end='\n' + '*' * (i == row and row * 2-1))
+
 '''
 Output 10
          *
